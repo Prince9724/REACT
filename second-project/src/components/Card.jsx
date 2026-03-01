ed3b7f171daa573d125bd3085d8f983be506b55f
Prince974 <princegondrw123@gmail.com>
2026-03-01 06:56:28 +0530

    ..

--- second-project/src/components/Card.jsx
+++ second-project/src/components/Card.jsx
@@ -2,14 +2,18 @@ import { product } from '../products.js';
 const Card = () => {
   return (
     <>
-    {
-      product.map((item,i)=> <div className='card' key={product.id}>
-        
-          <p className='title'>{product[i].title}</p>
-        <p className='price'>{product[i].price}</p>
+      {
+        product.map((item, i) =>
+           
+            <div className='card' key={product.id}>
+              <img className='product-img' src={product[i].images} alt="" />
+              <p className='title'>{product[i].title}</p>
+              <p className='price'>price- {product[i].price}-/</p>
 
-      </div>)
-    }
+            </div>
+           
+        )
+      }
     </>
   )
 }
