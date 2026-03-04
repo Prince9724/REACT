360bdd7e9d34d191a282ffb3c2b2fcec32d221cb
Prince974 <princegondrw123@gmail.com>
2026-03-04 20:28:18 +0530

    ...

--- utils-text/src/components/Text-utils.jsx
+++ utils-text/src/components/Text-utils.jsx
@@ -1,35 +1,67 @@
-import {useState} from 'react'
+import { useEffect, useState } from 'react'
 
 const TextUtils = () => {
-    const [text, settext] = useState("");
-    const [count, setcount] = useState(0);
-    const [wordcount, setwordcount] = useState(0);
-    const [readcount, setreadcount] = useState(0);
+  useEffect(() => {
+    // console.log("true")
+    settext(localStorage.getItem("text")?? "")
+    setcount(localStorage.getItem("count")?? 0)
+    setwordcount(localStorage.getItem("wordcount")?? 0)
+    setreadcount(localStorage.getItem("read")?? 0)
+  },[])
+
+  const [text, settext] = useState("");
+  const [count, setcount] = useState(0);
+  const [wordcount, setwordcount] = useState(0);
+  const [readcount, setreadcount] = useState(0);
   return (
     <>
       <div className='container'>
         <div className="form-floating">
-          <textarea onChange={(e)=>settext(e.target.value) } className="form-control" style={{height: 200}} placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+          <textarea value={text} onChange={(e) => settext(e.target.value)} className="form-control" style={{ height: 200 }} placeholder="Leave a comment here" id="floatingTextarea"></textarea>
           <label htmlFor="floatingTextarea">Enter you text here..</label>
         </div>
         <div>
-          <button onClick={()=>settext(text.toUpperCase())} className='btn btn-primary mt-3'>Upper Case</button>
-          <button onClick={()=>settext(text.toLowerCase())} className='btn btn-warning ms-3 mt-3'>Lower case</button>
-          <button onClick={()=>setcount(text.length)} className='btn btn-info ms-3 mt-3'>Count letters</button>
-          <button onClick={()=>setwordcount(text.split(" ").length)} className='btn btn-success   ms-3 mt-3'>Count letters</button>
-          <button onClick={()=>setreadcount(((text.split(" ").length*60)/180 ).toFixed(2))} className='btn btn-secondary   ms-3 mt-3'>Reading-time</button>
-          <button onClick={()=>setreadcount()} className='btn btn-secondary   ms-3 mt-3'>Reading-time</button>
+          <button onClick={() => {
+            settext(text.toUpperCase())
+            localStorage.setItem("text", text.toUpperCase())
+          }} className='btn btn-primary mt-3'>Upper Case</button>
+          <button onClick={() => {
+            settext(text.toLowerCase())
+            localStorage.setItem("text", text.toLowerCase());
+          }} className='btn btn-warning ms-3 mt-3'>Lower case</button>
+          <button onClick={() => {
+            setcount(text.length)
+            localStorage.setItem("count", text.length.toString());
+          }} className='btn btn-info ms-3 mt-3'>Count letters</button>
+          <button onClick={() => {
+            const count =(text.split(" ").length.toString())
+            setwordcount(count);
+            localStorage.setItem("wordcount", count)
+          }} className='btn btn-success   ms-3 mt-3'>Count letters</button>
+          <button onClick={() => {
+            setreadcount(((text.split(" ").length * 60) / 180).toFixed(2))
+            localStorage.setItem("read", readcount)
+          }} className='btn btn-secondary   ms-3 mt-3'>Reading-time</button>
+          <button onClick={() => {
+            settext(" ");
+            setcount(0)
+            setwordcount(0)
+            setreadcount(0)
+            localStorage.clear()
+          }
+
+          } className='btn btn-danger ms-3 mt-3'>Clear</button>
 
         </div>
         <hr />
         <div className='mt-3'>
-        <p>
-          {text}
-          
-        </p>
-        <p>count: {count}</p>
-        <p>word count: {wordcount}</p>
-        <p> Reading timing: {readcount}s</p>
+          <p>
+            {text}
+
+          </p>
+          <p>count: {count}</p>
+          <p>word count: {wordcount}</p>
+          <p> Reading timing: {readcount}s</p>
         </div>
       </div>
     </>
