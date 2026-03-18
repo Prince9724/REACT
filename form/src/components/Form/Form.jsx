77d1300465a3233b646e1601d4414084898c5ef0
Prince974 <princegondrw123@gmail.com>
2026-03-18 20:34:05 +0530

    ..

--- form/src/components/Form/Form.jsx
+++ form/src/components/Form/Form.jsx
@@ -1,42 +1,66 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
-
+import { useNavigate } from 'react-router';
 const Form = () => {
-    const [user, setUser] =useState({})
-    const handlleSubmite =()=>{
-        localStorage.setItem("user",JSON.stringify(user));//local storage ke ander user name 
-        // ki key pr user ki puri information store hai. string form me.  
+    const navigate = useNavigate();
+    const [user, setUser] = useState({})
+    const [check, setcheck] = useState(false)
+    const [eye, seteye] = useState(false);
+    const handlleSubmite = (e) => {
+        e.preventDefault();
+        const paswordResex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if (user.email == undefined || user.email == " ") {
+            alert("email is requared !!")
+        }
+        else if (user.name == " " || user.name == undefined) {
+            alert("Name is requared !!")
+        }
+        else if (!paswordResex.test(user.pasword) || user.pasword == " ") {
+            alert(" Pasword is requared !!");
+        }
+
+        else {
+            const users = JSON.parse(localStorage.getItem("users")) || [];//local storage se user name ki array ko get kr liya hai 
+            users.push(user);// users ke ander nai user ki ki value push ho rhi hai aur vo local storage ke ander set ho rhi hai 
+            localStorage.setItem("users", JSON.stringify(users));//local storage ke ander user name 
+            // ki key pr user ki puri information store hai. string form me.  
+            navigate("/user")
+        }
+
+
 
     }
-    
+
     return (
-        <div style={{height:"100vh"}} className=' container d-flex justify-content-center align-items-center'>
+        <div style={{ height: "100vh" }} className=' container d-flex justify-content-center align-items-center'>
             <form onSubmit={handlleSubmite}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">
-                       Your Name
+                        Your Name
                     </label>
                     <input
-                    onChange={(e)=>setUser({...user, name:e.target.value})}
+                        onChange={(e) => setUser({ ...user, name: e.target.value })}
                         type="text"
                         className="form-control"
                         id="name"
                         aria-describedby="emailHelp"
                     />
-                    
+
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">
                         Email address
                     </label>
                     <input
-                    onChange={(e)=>setUser({...user, email:e.target.value})}
+                        onChange={(e) => setUser({ ...user, email: e.target.value })}
 
                         type="Email"
                         className="form-control"
                         id="email"
                         aria-describedby="emailHelp"
                     />
+
                     <div id="emailHelp" className="form-text">
                         We'll never share your email with anyone else.
                     </div>
@@ -46,28 +70,38 @@ const Form = () => {
                         Password
                     </label>
                     <input
-                        type="Password"
+                        type= {eye ?"text": "password"}
                         className="form-control"
                         id="pasword"
-                    onChange={(e)=>setUser({...user, pasword:e.target.value})}
+                        onChange={(e) => setUser({ ...user, pasword: e.target.value })}
 
                     />
+                    <span  >
+                        <i onClick={() => seteye(!eye)} className="ri-eye-line"></i>
+                    </span>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">
-                       Contact
+                        Contact
                     </label>
                     <input
                         type="number"
                         className="form-control"
-                        id="exampleInputEmail1"
+                        id="contact"
                         aria-describedby="emailHelp"
+                        onChange={(e) => setUser({ ...user, contact: e.target.value })}
+
                     />
-                    
+
                 </div>
-               
-                 
-                <button  type="submit" className="btn btn-primary">
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" checked={check} onChange={() => setcheck(!check)} id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">
+                        Check me out
+                    </label>
+                </div>
+
+                <button type="submit" className={`btn btn-primary ${check ? " " : "disabled"}`}>
                     Submit
                 </button>
             </form>
@@ -76,5 +110,5 @@ const Form = () => {
         </div>
     )
 }
-
+ 
 export default Form
--- form/src/components/user/User.jsx
+++ form/src/components/user/User.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import { useState } from 'react'
 const User = () => {
-  return (
-    <div>
-        <h1>User</h1>
-    </div>
-  )
+    const [users, setusers] = useState(JSON.parse(localStorage.getItem("users")) || [])
+    return (
+        <div className='container d-flex flex-wrap justify-content-center gap-3 mt-5'>
+
+            {
+                users.map((user, i) => (
+                    <div className=' shadow rounded p-3 m-3'>
+                        <h1 key={i}>{user.name}</h1>
+                        <p key={i}>{ user.email} </p>
+                        <h4 key={i}>  {user.pasword}</h4>
+                        <h5 key={i}>{user.contact}</h5>
+                        <div>
+                            <button className='btn btn-danger'>delete</button>
+                        </div>
+                    </div>
+                     
+                ))
+            }
+        </div>
+
+    )
 }
 
 export default User
