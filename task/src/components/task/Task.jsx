41abc6f0ff6702371baea5c7c3f45708e96a3565
Prince974 <princegondrw123@gmail.com>
2026-03-30 15:56:08 +0530

    ...

--- task/src/components/task/Task.jsx
+++ task/src/components/task/Task.jsx
@@ -3,34 +3,37 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 
 const Task = () => {
-    const [task, settask] = useState({});
+    const [task, settask] = useState({
+        title: "",
+        description :""
+    });
     const handlesubmit =()=>{
-        let displayArr = JSON.parse(localStorage.getItem("task"))||[];
+        const displayArr = JSON.parse(localStorage.getItem("task"))||[];
         displayArr.push(task);
-        localStorage.setItem("task",JSON.stringify(task));
+       localStorage.setItem("task", JSON.stringify(displayArr));
         console.log(task.title);
     }
-    useEffect(()=>{
-        handlesubmit();
-    },[])
+    // useEffect(()=>{
+    //     handlesubmit();
+    // },[])
     return (
         <div className='container-fluid'>
             <div className="container ">
                 <div className='d-flex flex-wrap gap-3 justify-content-center mt-5 border border-2 shadow p-3 rounded-2'>
                     <input type="text" className='rounded'onChange={(e)=> settask({...task , title : e.target.value})} /><label htmlFor="" className='fs-5'>tittle</label>
                     <input type="text" className='rounded'onChange={(e)=> settask({...task , description : e.target.value})} /><label htmlFor="" className='fs-5'>Description</label>
-                    <select class="form-select"className='w-25' aria-label="Default select example">
+                    {/* <select class="form-select"className='w-25' aria-label="Default select example">
                         <option selected>Select Your status</option>
                         <option value="1">Complete</option>
                         <option value="2">Running</option>
                         <option value="3">Pending</option>
-                    </select>
+                    </select> */}
                     <button onClick={handlesubmit} className='btn btn-primary'>
                         Add
                     </button>
-                    <button className='btn btn-primary'>
-                        Add
-                    </button>
+                    {/* <button className='btn btn-primary'>
+                        
+                    </button> */}
                 </div>
             </div>
             <div className="container">
