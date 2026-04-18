0c7233241931cfc6a5ba7a0bdaa281ef36f3f16d
Prince974 <princegondrw123@gmail.com>
2026-04-18 18:18:29 +0530

    ....

--- todo-rtk/src/screen/Todo.jsx
+++ todo-rtk/src/screen/Todo.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import { useSelector,useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { addTodo, removeTodo, updateTodo } from '../feature/todoSlice'
-import { useState } from 'react'
+import './Todo.css'
+
 const Todo = () => {
-    const[text,settext] = useState("");
-    const  data = useSelector((state)=>state.todo.value)
-    const dispatch = useDispatch();
+  const [text, settext] = useState("")
+  const data = useSelector((state) => state.todo.value)
+  const dispatch = useDispatch()
+
   return (
-    <div className='container'>
-        <div className='d-flex justify-content-center mt-5'>
-            <input type="text" 
-              value={text||""}
-            onChange={(e)=>settext(e.target.value)}/>
-            <button className='btn btn-secondary ms-3' onClick={()=>{
-              dispatch((addTodo(text)))
-              settext();  
-            }} >add</button>
+    <div className="todo-main">
+
+      <div className="todo-card">
+
+        <h1 className="title">My Todo App</h1>
+
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Enter task..."
+            value={text || ""}
+            onChange={(e) => settext(e.target.value)}
+          />
+          <button className="add-btn" onClick={() => {
+            dispatch(addTodo(text))
+            settext("")
+          }}>
+            Add
+          </button>
         </div>
-        <div className='mt-5'>
+
+        <div className="todo-list">
           {
-            data.map((e,i)=><div className='border border-danger mt-3 border-2 w-50 d-flex justify-content-between p-3 rounded rounded-3'>
-          <h2>{e}</h2>
-          <button className='btn btn-outline-danger' onClick={()=>dispatch(removeTodo(i))}>remove</button>
-          <button className='btn btn-outline-success'onClick={()=>dispatch(updateTodo())}>
-            Edit
-          </button>  
-            </div>)
+            data.map((e, i) => (
+              <div className="todo-item" key={i}>
+
+                <h3 className="todo-text">{e}</h3>
+
+                <div className="actions">
+                  <button className="edit" onClick={() => dispatch(updateTodo(i))}>
+                    Edit
+                  </button>
+                  <button className="delete" onClick={() => dispatch(removeTodo(i))}>
+                    Delete
+                  </button>
+                </div>
+
+              </div>
+            ))
           }
         </div>
+
+      </div>
 
     </div>
   )
