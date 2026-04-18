2bf99f745940e47fada611581721689483c2ca23
Prince974 <princegondrw123@gmail.com>
2026-04-18 17:21:20 +0530

    ...

--- todo-rtk/src/screen/Todo.jsx
+++ todo-rtk/src/screen/Todo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector,useDispatch } from 'react-redux'
-import { addTodo, removeTodo } from '../feature/todoSlice'
+import { addTodo, removeTodo, updateTodo } from '../feature/todoSlice'
 import { useState } from 'react'
 const Todo = () => {
     const[text,settext] = useState("");
@@ -22,7 +22,9 @@ const Todo = () => {
             data.map((e,i)=><div className='border border-danger mt-3 border-2 w-50 d-flex justify-content-between p-3 rounded rounded-3'>
           <h2>{e}</h2>
           <button className='btn btn-outline-danger' onClick={()=>dispatch(removeTodo(i))}>remove</button>
-
+          <button className='btn btn-outline-success'onClick={()=>dispatch(updateTodo())}>
+            Edit
+          </button>  
             </div>)
           }
         </div>
