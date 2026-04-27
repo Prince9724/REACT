fa40563f2798b76b274a173884e9cb5d8fecc78e
Prince974 <princegondrw123@gmail.com>
2026-04-27 18:24:50 +0530

    ...

--- rtk-asyncthunk/src/components/User.jsx
+++ rtk-asyncthunk/src/components/User.jsx
@@ -2,15 +2,23 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchUser } from '../feature/userSlice'
 const User = () => {
+    const {user , status,error} = useSelector(state =>state.user)
+//    const dispatch = useDispatch()
+    const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(fetchUser())
-    }, [])
-    const dispatch = useDispatch()
+    }, [dispatch])
     return (
         <div>
             {
-                
+               user.map((u , i)=><div key={i}>
+                    {u.name};
+                   
+                </div> )
             }
+             {status}
+             {error}
         </div>
     )
 }
