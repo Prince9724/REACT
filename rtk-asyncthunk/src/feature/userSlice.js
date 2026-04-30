275ab69c722da0a977012df4502aad2ab6241d58
Prince974 <princegondrw123@gmail.com>
2026-04-30 15:50:57 +0530

    ..

--- rtk-asyncthunk/src/feature/userSlice.js
+++ rtk-asyncthunk/src/feature/userSlice.js
@@ -23,7 +23,7 @@ const userSlice = createSlice({//
     extraReducers: (builder) => {
 
         builder.addCase(fetchUser.pending, (state) => {
-            state.status = "loading"
+            state.status = "Ruko jra sbr kro.."
         }),
             builder.addCase(fetchUser.fulfilled, (state, action) => {
                 state.user = action.payload
