69cd7194733313794bfad39b039ece236cfb5f51
Prince gond <princegondrw123@gmail.com>
2026-05-21 15:24:28 +0530

    ...

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -1,8 +1,12 @@
 
 import React from 'react'
+import Home from './components/Home'
 
-export const App = () => {
+ const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
+export default App;
--- todo_app/src/components/Home.jsx
+++ todo_app/src/components/Home.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import {useDispatch, useSelector}from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import store from '../store/store'
+import { addTodo, getTodo, updateTodo, deleteTodo } from '../feature/todoSlice'
 const Home = () => {
+  const todos = useSelector((state) => state.tasks.todos)
+  const dispatch = useDispatch()
+  const [todo, settodo] = useState({
+    title: "",
+    description: ""
+  })
+  const handlePost = () => {
+    useEffect(() => {
+      dispatch(getTodo());
+    },[dispatch])
+  }
   return (
     <div className='container'>
+      <div>
+        <input className='m-3' type="text" placeholder='tittle' onChange={(e) => settodo({ ...todo, title: e.target.value })} />
+        <input className='m-3' type="text" placeholder='description.' onChange={(e) => settodo({ ...todo, description: e.target.value })} />
+        <button onClick={
+          () => {
+            if (todo.title == "" && todo.description == "") {
+              alert("please fill both side .. ")
+            }
+            else {
+              dispatch(addTodo(todo));
 
+            }
+
+          }
+        } className='btn btn-secondary' >Add</button>
+      </div>
+      <div className='container'>
+        <div className='todo-box d-flex flex-wrap w-100'>
+          {
+            todos.map((todo ,id) => (
+              <div key={id} className='todo'>
+                <p >{todo.title}</p>
+                <p >{todo.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Home
-//https://chatgpt.com/c/69dde41b-c804-8323-9957-0e9aa4be576b
